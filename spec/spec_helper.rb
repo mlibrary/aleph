f8366e7db5e62c3ef17c35bcf912171ed1c72f67
Morten Rønne <morten.roenne@tdcadsl.dk>
2013-04-03 08:00:08 +0200

    Initial Riyosha rails application.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,37 @@
+require 'simplecov'
+require 'simplecov-html'
+require 'simplecov-rcov'
+
+class SimpleCov::Formatter::MergedFormatter
+  def format(result)
+     SimpleCov::Formatter::HTMLFormatter.new.format(result)
+     SimpleCov::Formatter::RcovFormatter.new.format(result)
+  end
+end
+SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
+SimpleCov.start 'rails'
+
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'mocha'
+require 'factory_girl_rails'
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.use_transactional_fixtures = true
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
+
+  config.infer_base_class_for_anonymous_controllers = false
+  config.mock_with :mocha
+end
+
+#Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
