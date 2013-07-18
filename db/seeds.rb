aad1d493c6f952273b8639c01e57112a90c795aa
Morten Rønne <=>
2013-07-18 12:34:33 +0200

    Updated db seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db
+# with db:setup).
 #
 # Examples:
 #
@@ -8,7 +10,7 @@
 [
   { code: 'student' },
   { code: 'private' },
-  { code: 'dtuempl' },
+  { code: 'dtu_empl' },
 ].each do |usertype|
   UserType.find_or_create_by_code(usertype)
 end
