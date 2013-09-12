d00a6cafff166f5b956cd644ff04ab8bd9e08a8a
Morten Rønne <mron.dtic.dtu.dk>
2013-09-12 12:29:48 +0200

    Added flash clear to logout action.

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,12 +4,14 @@ require 'dtubase'
 class Users::SessionsController < Devise::SessionsController
 
   def new
+    session[:template] = params[:template] if params[:template] 
+    session[:template] ||= 'local_user'
     if (user = ticket_valid(params['ticket']))
       sign_in_and_redirect user, :event => :authentication
     elsif params[:only] == 'dtu'
       redirect_to dtu_login_url
     else
-      @login_template = params[:template] || 'local_user'
+      @login_template = session[:template]
       super
     end
   end
@@ -27,7 +29,6 @@ class Users::SessionsController < Devise::SessionsController
     return false if ticket.nil? || ticket.blank?
 
     # Use Ruby Cas Client for validation
-    @login_template = 'dtu_user'
     st = CASClient::ServiceTicket.new(ticket, url_for(:only_path => false))
     cas_client.validate_service_ticket(st)
     if st.is_valid?
@@ -44,37 +45,9 @@ class Users::SessionsController < Devise::SessionsController
     end
   end
 
-  def x_new
-    if (params['clear_checked'])
-      session[:checked_in_dtu_base] = false
-      session[:fake_login] = nil
-    end
-    session[:fake_login] = params['fake_login'] if !Rails.env.production?
-
-    # Set default template
-    @login_template = session[:template] || 'local_user'
-    if (user = ticket_valid(params['ticket']))
-      logger.info "Redirect sign_in"
-      sign_in_and_redirect user, :event => :authentication
-    elsif send_to_dtu
-      redirect_to dtu_login_url
-    elsif !session[:checked_in_dtu_base]
-      session[:checked_in_dtu_base] = true
-      logger.info "Redirect to #{dtu_login_url}"
-      redirect_to dtu_login_url+"&gateway=true"
-    else
-      case params['template']
-      when 'dtu_user'
-        @login_template = params['template']
-      end
-      session[:template] = @login_template
-      super
-    end
+  def destroy
+    super
+    flash.clear
   end
-
-  def send_to_dtu
-    params['only'] == 'dtu'
-  end
-
 
 end
