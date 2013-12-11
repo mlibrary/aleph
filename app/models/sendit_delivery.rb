7422a8158ca19b1ed8d8cbe07a878e04e05fe833
Morten Rønne <mron@dtic.dtu.dk>
2013-12-11 10:34:40 +0100

    Changed log message to include full url not just subject. Added log message about exception message.

--- app/models/sendit_delivery.rb
+++ app/models/sendit_delivery.rb
@@ -12,7 +12,7 @@ class SenditDelivery
       # Craft url with template name
       url = "#{config.sendit_url}/send/#{mail.subject}"
       response = HTTParty.post url, {
-        :body => mail.body,
+        :body => mail.body.to_s,
         :headers => { 'Content-Type' => 'application/json' }
       }
       unless response.code == 200
@@ -20,8 +20,9 @@ class SenditDelivery
         raise "Error communicating with SendIt"
       end
     rescue StandardError => e
-      logger.error "Error sending mail: template = #{mail.subject} "+
+      logger.error "Error sending mail: url = #{url} "+
         "- body #{mail.body}"
+      logger.error "Exception #{e.message}"
       raise e
     end
   end
